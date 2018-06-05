9e226128d19ea7441ea27defd7d85638f65ae1f8
zaki <yamawork1450@gmail.com>
2018-06-05 06:26:30 +0000

    tasklist top page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.page(params[:page])
+  end
+
+  def show
+     @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  root to: 'toppages#index'
+  
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'tasks#index'
   
-  resources :tasks
+  get 'signup', to: 'users#new'
+  resources :tasks, only: [:index, :show, :new, :create]
 end
