ee3e6f208d25424a4038c147c4a6918895c89dad
zaki <yamawork1450@gmail.com>
2018-06-05 07:06:09 +0000

    tasklist user login

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do
   gem 'pg', '0.21.0'
 end
+
+gem 'kaminari'
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
 
   root to: 'toppages#index'
-  
-  get 'users/index'
 
-  get 'users/show'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
-  get 'users/new'
-
-  get 'users/create'
-
-  root to: 'tasks#index'
-  
   get 'signup', to: 'users#new'
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
 end
